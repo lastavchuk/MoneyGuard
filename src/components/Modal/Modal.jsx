57052f782e9b8bbda2023ea2de6a19211f349267
Andrii Lutsiak <timechain04@gmail.com>
2023-08-03 20:11:50 +0300

    styled

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,43 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { createPortal } from 'react-dom';
-const modalContainer = document.getElementById('modal');
+import { useEffect } from 'react';
+import { Backdrop, ExitModalBtn, IconCloseModal, ModalStyled } from './Modal.styled';
 
-export const Modal = ({ children }) => {
-  return createPortal(<div>{children}</div>, modalContainer);
-};
+const modalWrapper = document.getElementById('modal');
+export const Modal = ({ children, closeReducer }) => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const onCloseModalEsc = evt => {
+            if (evt.code === 'Escape') {
+             dispatch(closeReducer());
+            }
+        };
+        window.addEventListener('keydown', onCloseModalEsc);
+        return() => {
+            window.removeEventListener('keydown', onCloseModalEsc);
+        };
+    }, [closeReducer, dispatch]);
+    const onCloseModal = evt => {
+        if(evt.currentTarget === evt.target) {
+            dispatch(closeReducer());
+        }
+    };
+    const handleCloseModal = () => {
+        dispatch(closeReducer());
+    };
+    return createPortal(
+        <Backdrop onClick={onCloseModal}>
+          <ModalStyled>
+          <IconCloseModal onClick={handleCloseModal}>
+          <ExitModalBtn />
+          </IconCloseModal>{children}
+          </ModalStyled>
+        </Backdrop>,modalWrapper
+      );
+    };
 
 Modal.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
+    closeReducer: PropTypes.func.isRequired,
 };
--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,14 +1,57 @@
 import styled from 'styled-components';
+import { VscClose } from 'react-icons/vsc';
 
 export const Backdrop = styled.div`
+  background: rgba(34, 13, 91, 0.23);
+  backdrop-filter: blur(3.5px);
   width: 100vw;
   height: 100vh;
-  backdrop-filter: blur(3.5px);
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  overscroll-behavior: contain;
 `;
 
 export const IconCloseModal = styled.div`
   width: ${({ theme }) => theme.spacing(7)};
   height: ${({ theme }) => theme.spacing(7)};
-
+  position: absolute;
+  top: ${({ theme }) => theme.spacing(5)};
+  right: ${({ theme }) => theme.spacing(5)};
+  &:hover {
+    cursor: pointer;
+  }
 `;
+
+// Close Button Styles
+export const ExitModalBtn = styled(VscClose)`
+  width: ${({ theme }) => theme.spacing(7.4)};
+  height: ${({ theme }) => theme.spacing(7.4)};
+  &:hover {
+    border-radius: 50%;
+    border: 1px solid rgba(255, 255, 255, 0.4);
+  }
+`;
+
+export const ModalStyled = styled.div`
+  background: rgb(64, 46, 155);
+  background: radial-gradient(
+    circle,
+    rgba(64, 46, 155, 1) 5%,
+    rgba(76, 50, 113, 1) 100%
+  );
+  width: 100vw;
+  min-height: 110vh;
+  border-radius: ${({ theme }) => theme.spacing(2)};
+  padding: 40px 72px;
+
+  @media screen and (min-width: 768px) {
+    width: ${({ theme }) => theme.spacing(135)};
+    min-height: ${({ theme }) => theme.spacing(145)};
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+  }
+`;
+
