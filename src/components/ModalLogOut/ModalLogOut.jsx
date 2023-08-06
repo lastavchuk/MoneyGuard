646044cd32f308c8157ddde5e3265260bf6462eb
Andrii Lutsiak <timechain04@gmail.com>
2023-08-06 14:04:34 +0300

    redux

--- src/components/ModalLogOut/ModalLogOut.jsx
+++ src/components/ModalLogOut/ModalLogOut.jsx
@@ -1,15 +1,24 @@
 import { Button } from 'components/Button/Button';
 import React from 'react';
+import { ModalContainer, ButtonWrapper, ModalWraper, Title } from './ModalLogOut.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from 'redux/auth/authSelectors';
-import { closeModalLogout } from 'redux/global/globalSlice';
-import { ButtonWrapper, ModalContainer, ModalWraper, Title } from './ModalLogOut.styled';
+import { selectUser } from 'redux/selectors';
+import { logOutThunk } from 'redux/auth/userThunks';
+import { closeModalLogout } from 'redux/auth/userSlice';
+import { toast } from 'react-toastify';
 
 export const ModalLogOut = () => {
-  const user = useSelector(selectUser);
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
 
-
+  const handleLogOut = () => {
+    dispatch(logOutThunk())
+  .unwrap()
+  .then(data => {
+    toast.success(`You have successfully logged out. We hope to see you back soon!`);
+  });
+    dispatch(closeModalLogout());
+  };
   return (
     <ModalWraper>
       <ModalContainer>
@@ -21,6 +30,7 @@ export const ModalLogOut = () => {
             text="Cancel"
             variant={'secondary'}
           />
+          <Button type="button" onClick={handleLogOut} text="Yeah, Exit" />
         </ButtonWrapper>
       </ModalContainer>
     </ModalWraper>
