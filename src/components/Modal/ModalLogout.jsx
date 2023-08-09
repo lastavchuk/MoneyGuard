4977b9afe09235811203afae5a4616a18c84f180
lastavchuk <it.lastavchuk@gmail.com>
2023-08-09 14:49:48 +0300

    add modal exit

---
+++ src/components/Modal/ModalLogout.jsx
@@ -0,0 +1,52 @@
+import { Button } from 'components/Button/Button';
+import { useDispatch } from 'react-redux';
+import { logOutUserThunk } from 'redux/auth/userThunks';
+import { closeModalLogout } from 'redux/globalSlice';
+import {
+    ButtonWrapper,
+    LogotipStyled,
+    ModalContainer,
+    ModalWraper,
+    Title,
+} from './ModalLogout.styled';
+import Logotip from '../../assests/img/logo.svg';
+
+export const ModalLogout = () => {
+    const dispatch = useDispatch();
+
+    const handleLogOut = () => {
+        dispatch(logOutUserThunk());
+        dispatch(closeModalLogout());
+    };
+
+    return (
+        <ModalWraper>
+            <ModalContainer>
+                <LogotipStyled>
+                    <img
+                        src={Logotip}
+                        alt="Logo MoneyGuard"
+                        width="36px"
+                        height="36px"
+                    />
+                    <h3>MoneyGuard</h3>
+                </LogotipStyled>
+                <Title>Are you sure you want to log out?</Title>
+
+                <ButtonWrapper>
+                    <Button
+                        type="button"
+                        onClick={handleLogOut}
+                        text="Logout"
+                    />
+                    <Button
+                        type="button"
+                        onClick={() => dispatch(closeModalLogout())}
+                        text="Cancel"
+                        variant={'secondary'}
+                    />
+                </ButtonWrapper>
+            </ModalContainer>
+        </ModalWraper>
+    );
+};
