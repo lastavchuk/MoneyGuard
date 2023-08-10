11ae4cb8e795cee982994e02b4dd57f98b08b0a0
Leo007ua <leo.oliinyk@ukr.net>
2023-08-10 10:09:40 +0300

    add position footer

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -5,22 +5,34 @@ export const StyledFooter = styled.footer`
     display: flex;
     justify-content: center;
     align-items: center;
-    position: absolute;
+    /* position: absolute; */
     bottom: 0;
     width: 100%;
+
+    @media only screen and (min-width: 768px) {
+        position: absolute;
+        font-size: 17px;
+    }
+    
 `;
 export const FooterText = styled.p`
     color: var(--white-60);
-    /* font-size: 17px; */
+    font-size: 13px;
+    @media only screen and (min-width: 768px) {
+        font-size: 17px;
+    }
 `;
 
 export const FooterLink = styled.a`
     margin-left: 10px;
-    font-size: 17px;
+    font-size: 13px;
     color: white;
     /* color: var(--white-60); */
 
     &:hover {
         color: aquamarine;
+    }
+    @media only screen and (min-width: 768px) {
+        font-size: 17px;
     }
 `;
