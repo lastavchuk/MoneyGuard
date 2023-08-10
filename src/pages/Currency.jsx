f12abddd4f8a12112818692d5bc9a3c61d5127c1
lastavchuk <126959996+lastavchuk@users.noreply.github.com>
2023-08-10 13:09:58 +0300

    Revert "fix currency"

--- src/pages/Currency.jsx
+++ src/pages/Currency.jsx
@@ -1,10 +1,13 @@
-
+// import SideBalance from 'components/SideBar/SideBalance/SideBalance';
+import SideBalance from 'components/SideBar/SideBalance/SideBalance';
+import SideBar from 'components/SideBar/SideBar';
 import { Graphics, Line } from 'components/SideBar/SideBar.styled';
 import SideCurrency from 'components/SideBar/SideCurrency/SideCurrency';
 import SideNav from 'components/SideBar/SideNav/SideNav';
 import { useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { useNavigate} from 'react-router-dom';
+// import SideNav from 'components/SideBar/SideNav/SideNav';
 
 function Currency() {
 
